e8899910199118f55ec968609ea843da937bd048
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-08-12 00:28:48 +0530

    Sync: 2461. Maximum Sum of Distinct Subarrays With Length K (Medium) - Leet2Git

---
+++ 2461_Maximum_Sum_of_Distinct_Subarrays_With_Length_K.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        long long ans = 0;
+        long long sum = 0;
+
+        unordered_map<int, int> freq;
+        
+        int left = 0;
+        for(int right = 0; right < nums.size(); right++) {
+            freq[nums[right]]++;
+            sum += freq[nums[right]];
+            
+            while(right - left + 1 > k) {
+                freq[nums[left]]--;
+                sum -= nums[left];
+
+                if(freq[nums[left]] == 0)
+                    freq.erase(nums[left]);
+
+                left++;
+            }
+            // Window has exactly k elements
+            if(right - left + 1 == k && freq.size() == k) {
+                ans = max(ans, sum);
+            }
+        }
+        return ans;
+    }
+};
