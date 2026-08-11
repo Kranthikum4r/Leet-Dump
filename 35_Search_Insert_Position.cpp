66541d4fc2de87ecef06f72eba9d79be0946ee3e
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-08-11 23:28:01 +0530

    Sync: 35. Search Insert Position (Easy) - Leet2Git

--- 35_Search_Insert_Position.cpp
+++ 35_Search_Insert_Position.cpp
@@ -3,7 +3,7 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int low = 0, high = nums.size()-1;
 
-        int ans;
+        int ans = nums.size();
         while(low < high) {
             int mid = low + (high - low) / 2;
 
@@ -16,7 +16,7 @@ public:
                 low = mid+1;
             }
         }
-        
+
         return ans;
     }
 };
