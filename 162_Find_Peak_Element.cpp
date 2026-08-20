c72125df35009b53364c8e97c94501bfd3ff954e
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-08-20 23:33:56 +0530

    Sync: 162. Find Peak Element (Medium) - Leet2Git

--- 162_Find_Peak_Element.cpp
+++ 162_Find_Peak_Element.cpp
@@ -6,11 +6,11 @@ public:
         while(low < high) {
             int mid = low + (high - low) / 2;
 
-            if(nums[mid] > nums[high])
+            if(nums[mid] > nums[mid + 1])
                 high = mid;
             else
                 low = mid + 1;
         }
-        return low; // converges to one element
+        return high; // converges to one element
     }
 };
