9839837c4c77ed1d0f272ece1d9dc12a6d551021
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-08-23 00:21:25 +0530

    Sync: 1482. Minimum Number of Days to Make m Bouquets (Medium) - Leet2Git

--- 1482_Minimum_Number_of_Days_to_Make_m_Bouquets.cpp
+++ 1482_Minimum_Number_of_Days_to_Make_m_Bouquets.cpp
@@ -1,24 +1,52 @@
 class Solution {
 public:
+    bool canMake(vector<int>& bloomDay, int m, int k, int day) {
+        int boquets = 0;
+        int flowers = 0;
+
+        for(int x : bloomDay) {
+            if(x <= day) {
+                flowers++;
+
+                if(flowers == k) {
+                    boquets++;
+                    flowers = 0;
+                }
+            }
+            else {
+                flowers = 0;
+            }
+
+            if(boquets >= m) {
+                return true;
+            }
+        }
+        return false;
+    }
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n = bloomDay.size();
 
-        sort(bloomDay.begin(), bloomDay.end());
-        
-        int req = m * k;
-        if(n < req) return -1;
-        if(n == req) return bloomDay.back();
+        long long req = 1LL * m * k;
 
-        int l = 0, h = n - 1;
-        while(l <= h) {
+        if(n < req) return -1;
+
+        int l = *min_element(bloomDay.begin(), bloomDay.end());
+        int h = *max_element(bloomDay.begin(), bloomDay.end());
+
+        int ans = -1;
+
+        while(l < h) {
             int mid = l + (h - l) / 2;
 
-            if(mid + 1 == req) return bloomDay[mid];
-            else if(mid + 1 < req)
+            if(canMake(bloomDay, m, k, mid)) {
+                ans = mid;
+                h = mid - 1;
+            }
+            else {
                 l = mid + 1;
-            else
-                h = mid - 1;;
+            }
         }
+
         return -1;
     }
 };
