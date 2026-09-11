f44a437df9fc89c10a072ba5abf36e8223d2e62f
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-09-11 22:44:15 +0530

    Sync: 3483. Unique 3-Digit Even Numbers (Easy) - Leet2Git

--- 3483_Unique_3_Digit_Even_Numbers.cpp
+++ 3483_Unique_3_Digit_Even_Numbers.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
         int n = digits.size();
-        int ans = 0;
+        unordered_set<int> st;
 
         for(int i = 0; i < n; i++) {
             if(digits[i] % 2 != 0)
                 continue;
 
             for(int j = 0; j < n; j++) {
+                // j = hundreds digit
                 if(i == j)
                     continue;
 
@@ -16,13 +17,18 @@ public:
                     continue;
 
                 for(int k = 0; k < n; k++) {
+                    // k = tens digit
                     if(i == k || j == k)
                         continue;
                     
-                    ans++;
+                    int num = digits[j] * 100 +
+                              digits[k] * 10 +
+                              digits[i];
+
+                    st.insert(num);
                 }
             }
         }
-        return ans;
+        return st.size();
     }
 };
