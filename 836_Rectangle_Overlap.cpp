c9e0abcae64c755e93608a59180997d979a9e03d
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-09-14 12:06:05 +0530

    Sync: 836. Rectangle Overlap (Easy) - Leet2Git

--- 836_Rectangle_Overlap.cpp
+++ 836_Rectangle_Overlap.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        int dx = rec1[2] - rec2[0];
-        int dy = rec1[3] - rec2[1];
-    
-        return (dx >= 0 && dy >= 0 && (dx != 0 && dy != 0)) || (dx <= 0 && dy <= 0 && (dx != 0 && dy != 0));
+        return rec1[2] > rec2[0] &&
+               rec2[2] > rec1[0] &&
+               rec1[3] > rec2[1] &&
+               rec2[3] > rec1[1];
     }
 };
