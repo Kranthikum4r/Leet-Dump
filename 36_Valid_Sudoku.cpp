b313357038ac2cae02858fb9c6a76da5fb8d9419
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-09-17 18:22:04 +0530

    Sync: 36. Valid Sudoku (Medium) - Leet2Git

--- 36_Valid_Sudoku.cpp
+++ 36_Valid_Sudoku.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int r = board.size();
-        int c = board[0].size();
         for(int i = 0; i < 9; i++) {
             unordered_set<int> s;
             for(int j = 0; j < 9; j++) {
+                if(board[i][j] == '.') continue;
                 if(s.count(board[i][j])) {
                     return false;
                 }
-                else {
-                    s.insert(board[i][j]);
-                }
+                s.insert(board[i][j]);
             }
         }
         for(int i = 0; i < 9; i++) {
             unordered_set<int> s;
             for(int j = 0; j < 9; j++) {
+                if(board[j][i] == '.') continue;
                 if(s.count(board[j][i])) {
                     return false;
                 }
-                else {
-                    s.insert(board[j][i]);
-                }
+                s.insert(board[j][i]);
             }
         }
 
@@ -30,14 +26,13 @@ public:
             for(int j = 0; j < 9; j += 3) {
 
                 unordered_set<int> s;
-
                 for(int r = i; r < i + 3; r++) {
                     for(int c = j; c < j + 3; c++) {
-
+                        if(board[i][j] == '.') continue;
+                        
                         if(s.count(board[r][c])) {
                             return false;
                         }
-
                         s.insert(board[r][c]);
                     }
                 }
