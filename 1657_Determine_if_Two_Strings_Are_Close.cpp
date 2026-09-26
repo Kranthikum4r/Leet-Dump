cfbb7cc7004449877a438f86a6b507c4d3f0452c
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-09-26 16:46:59 +0530

    Sync: 1657. Determine if Two Strings Are Close (Medium) - Leet2Git

--- 1657_Determine_if_Two_Strings_Are_Close.cpp
+++ 1657_Determine_if_Two_Strings_Are_Close.cpp
@@ -3,16 +3,23 @@ public:
     bool closeStrings(string word1, string word2) {
         if(word1.size() != word2.size()) return false;
 
-        unordered_map<char, int> mp;
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
 
-        for(char c : word1) {
-            mp[c]++;
+        for (char c : word1)
+            freq1[c - 'a']++;
+
+        for (char c : word2)
+            freq2[c - 'a']++;
+
+        for(int i = 0; i < 26; i++) {
+            if((freq1[i] == 0) != (freq2[i] == 0))
+                return false;
         }
 
-        for(char c : word2) {
-            if(!mp.count(c)) return false;
-        }
+        sort(freq1.begin(), freq1.end());
+        sort(freq2.begin(), freq2.end());
 
-        return true;
+        return freq1 == freq2;
     }
 };
