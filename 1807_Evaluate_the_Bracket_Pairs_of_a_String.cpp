ca407c4796504811da330f4a570eaa4f17d540fb
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-09-26 16:36:26 +0530

    Sync: 1807. Evaluate the Bracket Pairs of a String (Medium) - Leet2Git

---
+++ 1807_Evaluate_the_Bracket_Pairs_of_a_String.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string, string> mp;
+        string ans = "";
+
+        for(auto& pair : knowledge) {
+            mp[pair[0]] = pair[1];
+        }
+
+        int i = 0;
+        int n = s.size();
+        while(i < n) {
+            if(s[i] == '(') {
+                string word = "";
+                while(s[i] != ')') {
+                    word += s[i];
+                    i++;
+                }
+                if(!mp.count(word)) {
+                    ans += '?';
+                }
+                else {
+                    ans += mp[word];
+                }
+            }
+            else {
+                ans += s[i];
+            }
+            i++;
+        }
+        return ans;
+    }
+};
